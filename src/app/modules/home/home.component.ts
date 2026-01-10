f985c140c5ae094f8e3e757862012c05e071c704
Shoriful Habib <shorifulhabib.iit@gmail.com>
2026-01-10 20:01:16 +0600

    feat: home page start

---
+++ src/app/modules/home/home.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class HomeComponent {}
---
+++ src/app/modules/home/home.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home.component';
+import { HomePage } from './pages/home-page/home-page';
+
+export default [
+  {
+    path: '',
+    component: HomePage,
+  },
+];
