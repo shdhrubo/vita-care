64c4eabed48ecf19cfa1b2df67581408b2496981
Shoriful Habib <shorifulhabib.iit@gmail.com>
2026-01-16 20:29:30 +0600

    feat: navbar added

--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.scss',
+  standalone: true,
 })
 export class Header {
+  isMenuOpen = false;
 
+  navLinks = [
+    { label: 'Home', path: '/' },
+    { label: 'Services', path: '/services' },
+    { label: 'Doctors', path: '/doctors' },
+    { label: 'About', path: '/about' },
+    { label: 'Contact', path: '/contact' },
+  ];
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
 }
