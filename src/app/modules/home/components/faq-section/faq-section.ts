6e814ecb4ae210f0e26137dc21fcb08f531ba16f
Shoriful Habib <shorifulhabib.iit@gmail.com>
2026-01-18 12:22:42 +0600

    some changes

---
+++ src/app/modules/home/components/faq-section/faq-section.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-faq-section',
+  imports: [],
+  templateUrl: './faq-section.html',
+  styleUrl: './faq-section.scss',
+})
+export class FaqSection {
+
+}
--- src/app/modules/home/containers/home-page-container/home-page-container.ts
+++ src/app/modules/home/containers/home-page-container/home-page-container.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { BannerSection } from '../../components/banner-section/banner-section';
+import { FaqSection } from '../../components/faq-section/faq-section';
 
 @Component({
   selector: 'app-home-page-container',
-  imports: [BannerSection],
+  imports: [BannerSection, FaqSection],
   templateUrl: './home-page-container.html',
   styleUrl: './home-page-container.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
