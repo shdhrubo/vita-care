377646977259f1f6b50e3ccbbc133d5fd3a5b7a8
Shoriful Habib <shorifulhabib.iit@gmail.com>
2026-01-18 12:36:14 +0600

    feat: some changes

--- src/app/modules/home/containers/home-page-container/home-page-container.ts
+++ src/app/modules/home/containers/home-page-container/home-page-container.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { BannerSection } from '../../components/banner-section/banner-section';
 import { FaqSection } from '../../components/faq-section/faq-section';
+import { TestimonialSection } from '../../components/testimonial-section/testimonial-section';
 
 @Component({
   selector: 'app-home-page-container',
-  imports: [BannerSection, FaqSection],
+  imports: [BannerSection, FaqSection, TestimonialSection],
   templateUrl: './home-page-container.html',
   styleUrl: './home-page-container.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
