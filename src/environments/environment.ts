c2596b6e47ec7a592a038c0657f79820ac154530
Shoriful Habib <shorifulhabib.iit@gmail.com>
2026-01-25 15:04:50 +0600

    auth 0 setup

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  auth0: {
+    domain: 'dev-aikn7b2k0kudugzk.us.auth0.com',
+    clientId: 'Fd18UHvl1kdPMWPtIHKDEclW3MhxkCwX',
+  },
+};
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { mergeApplicationConfig } from '@angular/core';
+import { environment } from './environments/environment';
+import { provideAuth0 } from '@auth0/auth0-angular';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+const auth0Config = mergeApplicationConfig(appConfig, {
+  providers: [
+    provideAuth0({
+      domain: environment.auth0.domain,
+      clientId: environment.auth0.clientId,
+      authorizationParams: {
+        redirect_uri: window.location.origin,
+      },
+    }),
+  ],
+});
+
+bootstrapApplication(App, auth0Config).catch((err) => console.error(err));
