7672b11c683452025f347a120bdc03cc338c4be7
Shoriful Habib <shorifulhabib.iit@gmail.com>
2026-03-26 19:35:38 +0600

    some changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
   apiBaseUrl: 'http://localhost:5198',
+  // apiBaseUrl: 'https://vita-care-backend.onrender.com',
   auth0: {
     domain: 'dev-aikn7b2k0kudugzk.us.auth0.com',
     clientId: 'Fd18UHvl1kdPMWPtIHKDEclW3MhxkCwX',
